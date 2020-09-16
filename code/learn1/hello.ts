b5d6974fb19009c515752db968bd86695aa78d72
li <lwaisyj@163.com>
2020-09-16 18:25:09 +0800

    add

---
+++ code/learn1/hello.ts
@@ -0,0 +1,10 @@
+import * as  file from 'https://deno.land/std@0.65.0/http/file_server.ts'
+console.log(file)
+
+console.log(Deno.cwd())
+
+async function read(path:string):Promise<any>{
+   
+}
+
+Deno.console.log(addEventListener)
